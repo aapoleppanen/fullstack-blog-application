1095979151cb017f59a40d8f44ce5632e729a1cb
Aapo Leppänen <aapo.leppanen@gmail.com>
2021-12-08 15:32:48 +0200

    added node version #skip

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const logger = require("./utils/logger");
 
 const server = http.createServer(app);
 
+console.log(server, "We have reached index.js");
+
 server.listen(config.PORT, () => {
 	logger.info(`Server running on port ${config.PORT}`);
 });
--- utils/config.js
+++ utils/config.js
@@ -7,8 +7,6 @@ const MONGODB_URI =
 		: process.env.MONGODB_URI;
 const SECRET = process.env.SECRET;
 
-console.log(PORT);
-
 module.exports = {
 	MONGODB_URI,
 	PORT,
