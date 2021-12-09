07f88736a72ba315e5e13ef5c4767998f5d22f17
Aapo Leppänen <aapo.leppanen@gmail.com>
2021-12-09 20:22:43 +0200

    changed ports in cypress commands #skip

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -30,7 +30,7 @@ Cypress.Commands.add("login", ({ username, password }) => {
 		password,
 	}).then(({ body }) => {
 		localStorage.setItem("blogAppLoggedUser", JSON.stringify(body));
-		cy.visit("http://localhost:3000");
+		cy.visit("http://localhost:3003");
 	});
 });
 
@@ -46,5 +46,5 @@ Cypress.Commands.add("createBlog", ({ title, author, url }) => {
 		},
 	});
 
-	cy.visit("http://localhost:3000");
+	cy.visit("http://localhost:3003");
 });
